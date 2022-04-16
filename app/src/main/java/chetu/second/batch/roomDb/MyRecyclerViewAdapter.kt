0fc4bf9b716fdd8a4154c8b1addb7ab9c10c116d
shub008 <tyagishubham0395@gmail.com>
2022-04-16 18:13:03 +0530

    insert/clear/recyclerview/adapter

---
+++ app/src/main/java/chetu/second/batch/roomDb/MyRecyclerViewAdapter.kt
@@ -0,0 +1,30 @@
+package chetu.second.batch.roomDb
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import chetu.second.batch.kotlinconcepts.databinding.ListItemsBinding
+import chetu.second.batch.roomDb.db.User
+
+class MyRecyclerViewAdapter(private val userList : List<User>) : RecyclerView.Adapter<MyViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflator = LayoutInflater.from(parent.context)
+        val binding = ListItemsBinding.inflate(layoutInflator, parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(userList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+}
+
+class MyViewHolder(val binding : ListItemsBinding) : RecyclerView.ViewHolder(binding.root){
+    fun bind(user: User){
+        binding.tvName.text = user.firstName+" "+user.lastName
+        binding.tvMobileNo.text = user.mobileNo
+    }
+}
--- app/src/main/java/chetu/second/batch/roomDb/view/UserDetailsActivity.kt
+++ app/src/main/java/chetu/second/batch/roomDb/view/UserDetailsActivity.kt
@@ -2,10 +2,15 @@ package chetu.second.batch.roomDb.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import chetu.second.batch.kotlinconcepts.R
 import chetu.second.batch.kotlinconcepts.databinding.ActivityUserDetailsBinding
+import chetu.second.batch.roomDb.MyRecyclerViewAdapter
 import chetu.second.batch.roomDb.db.UserDatabase
 import chetu.second.batch.roomDb.factory.UserDetailsViewModelFactory
 import chetu.second.batch.roomDb.repository.UserRepository
@@ -13,20 +18,28 @@ import chetu.second.batch.roomDb.viewModel.UserDetailsViewModel
 
 class UserDetailsActivity : AppCompatActivity() {
     lateinit var binding : ActivityUserDetailsBinding
+    lateinit var userViewModel : UserDetailsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_user_details)
+        binding.userRecyclerview.layoutManager = LinearLayoutManager(this)
 
         val dao = UserDatabase.getInstance(this).dao
         val userRepository = UserRepository(dao)
         val factory = UserDetailsViewModelFactory(userRepository)
-        val userViewModel = ViewModelProvider(this, factory)[UserDetailsViewModel::class.java]
+        userViewModel = ViewModelProvider(this, factory)[UserDetailsViewModel::class.java]
 
         binding.userViewModel = userViewModel
         binding.lifecycleOwner = this
 
+        displayUsersList()
 
+    }
 
+    fun displayUsersList(){
+        userViewModel.usersList.observe(this, Observer {
+            binding.userRecyclerview.adapter = MyRecyclerViewAdapter(it)
+        })
     }
 }
