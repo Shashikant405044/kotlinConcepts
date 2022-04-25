33883ab1317b5a71e4c5bc737dc9d73056dc543a
Rakesh <rakesh@avsstech.com>
2022-04-26 01:12:24 +0530

    Bug resolved

--- app/src/main/java/chetu/second/batch/mvvmCleanArchitecture/data/api/TMDBApi.kt
+++ app/src/main/java/chetu/second/batch/mvvmCleanArchitecture/data/api/TMDBApi.kt
@@ -1,11 +1,12 @@
 package chetu.second.batch.mvvmCleanArchitecture.data.api
 
 import chetu.second.batch.mvvmCleanArchitecture.data.model.Movie
+import chetu.second.batch.mvvmCleanArchitecture.data.model.MovieList
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TMDBApi {
    @GET("movie/popular")
-   suspend fun getMovie(@Query("api_key") apikey : String) : Response<List<Movie>>
+   suspend fun getMovie(@Query("api_key") apikey : String) : Response<MovieList>
 }
--- app/src/main/java/chetu/second/batch/mvvmCleanArchitecture/data/repository/CacheMovieDataSourceImpl.kt
+++ app/src/main/java/chetu/second/batch/mvvmCleanArchitecture/data/repository/CacheMovieDataSourceImpl.kt
@@ -3,7 +3,8 @@ package chetu.second.batch.mvvmCleanArchitecture.data.repository
 import chetu.second.batch.mvvmCleanArchitecture.data.model.Movie
 
 class CacheMovieDataSourceImpl : CacheMovieDataSource {
-    private lateinit var movieList : ArrayList<Movie>
+    private var movieList = ArrayList<Movie>()
+
     override suspend fun getMovieFromCache(): List<Movie> {
         return movieList
     }
